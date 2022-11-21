2ed8a964816905c1e4b7e21f532d51d7081e8e1b
AnastasiiaMaliuha <nasteamaluga@gmail.com>
2022-11-21 17:02:46 +0200

    add a github url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Werther</div>;
+  return (
+    <div className="App">
+      Werther
+      <p>
+        {" "}
+        Project created by Anastasiia Maliha, view at
+        <a
+          href="https://github.com/AnastasiiaMaliuha/anastasiia-react-app/settings"
+          target="_blank"
+        >
+          {" "}
+          GitHub
+        </a>
+      </p>
+    </div>
+  );
 }
